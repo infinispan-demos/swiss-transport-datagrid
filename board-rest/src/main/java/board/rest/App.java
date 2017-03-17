bcb27e9bd93526bd30f11c83baabd9a31f73a72d
Galder Zamarreño <galder@zamarreno.com>
2017-03-17 11:26:03 +0100

    Initial commit

---
+++ board-rest/src/main/java/board/rest/App.java
@@ -0,0 +1,13 @@
+package board.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
