a3fb45a2dcbc872b72fb373ca123ccbf875b1182
Galder Zamarreño <galder@zamarreno.com>
2017-04-20 10:00:23 +0200

    Add hostaccess policy for default user
    * This is required to be able to use hostPath volume.

--- datagrid/deploy.sh
+++ datagrid/deploy.sh
@@ -7,6 +7,10 @@ rm -drf target
 mkdir target
 cp ../analytics/analytics-server/target/analytics-server-1.0-SNAPSHOT.jar target/analytics-server.jar
 
+oc login -u system:admin
+oc adm policy add-scc-to-user hostaccess system:serviceaccount:myproject:default -n myproject
+
+oc login -u developer
 oc policy add-role-to-user view system:serviceaccount:myproject:default -n myproject
 oc create configmap server-configuration --from-file=./server-config.xml
 oc create -f ./datagrid.yml
